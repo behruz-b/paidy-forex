be1ad3c88addc235ec4fd76064208c888f846383
Behruz Boltayev <behruz.ravshonbekovich@gmail.com>
2024-02-06 13:40:48 +0500

    fix(unexpected-errors): Handled unexpected errors

--- src/main/scala/scala/forex/http/rates/QueryParams.scala
+++ src/main/scala/scala/forex/http/rates/QueryParams.scala
@@ -1,15 +1,20 @@
 package scala.forex.http.rates
 
+import cats.syntax.all.catsSyntaxEither
 import forex.domain.Currency
+import org.http4s.ParseFailure
 import org.http4s.QueryParamDecoder
 import org.http4s.dsl.impl.QueryParamDecoderMatcher
 
 object QueryParams {
-
-  private[http] implicit val currencyQueryParam: QueryParamDecoder[Currency] =
-    QueryParamDecoder[String].map(Currency.withName)
+  implicit private[http] val currencyQueryParam: QueryParamDecoder[Currency] =
+    QueryParamDecoder[String].emap(
+      str =>
+        Currency
+          .withNameEither(str.toUpperCase)
+          .leftMap(error => ParseFailure(s"Currency not found ${error.notFoundName}", error.getMessage()))
+    )
 
   object FromQueryParam extends QueryParamDecoderMatcher[Currency]("from")
   object ToQueryParam extends QueryParamDecoderMatcher[Currency]("to")
-
 }
--- src/main/scala/scala/forex/http/rates/RatesHttpRoutes.scala
+++ src/main/scala/scala/forex/http/rates/RatesHttpRoutes.scala
@@ -1,8 +1,10 @@
 package scala.forex.http
 package rates
 
+import scala.forex.programs.rates.Errors.Error.RateLookupFailed
 import cats.MonadThrow
 import cats.data.EitherT
+import cats.implicits.catsSyntaxApplicativeError
 import cats.syntax.flatMap._
 
 import forex.programs.RatesProgram
@@ -10,8 +12,9 @@ import forex.programs.rates.{ Protocol => RatesProgramProtocol }
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
+import org.typelevel.log4cats.Logger
 
-class RatesHttpRoutes[F[_]: MonadThrow](rates: RatesProgram[F]) extends Http4sDsl[F] {
+class RatesHttpRoutes[F[_]: MonadThrow: Logger](rates: RatesProgram[F]) extends Http4sDsl[F] {
   import QueryParams._
 
   private[http] val prefixPath = "/rates"
@@ -22,10 +25,16 @@ class RatesHttpRoutes[F[_]: MonadThrow](rates: RatesProgram[F]) extends Http4sDs
         .flatMap { rate =>
           Ok(rate)
         }
+        .recoverWith {
+          case error: RateLookupFailed =>
+            UnprocessableEntity(error.msg)
+          case error =>
+            Logger[F].error(error)("Error occurred while retrieve rates") >>
+              BadGateway("Something went wrong. Please try again.")
+        }
   }
 
   val routes: HttpRoutes[F] = Router(
     prefixPath -> httpRoutes
   )
-
 }
